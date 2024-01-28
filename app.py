e5c79cf874bb33dcc51a944415c0bae41412aadf
Mickael <mik3asg@gmail.com>
2024-01-28 23:46:45 +0000

    code updated beta

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'We are deploying a Flask app into AWS EKS Cluster --- BETA version.'
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
